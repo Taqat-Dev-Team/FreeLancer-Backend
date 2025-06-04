20fca808ee670b4f084d798b1469fcef4ac8f356
A7madjaber <aa20589@gmail.com>
2025-06-04 15:38:02 +0300

    fix login page style -deploy

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -35,67 +35,59 @@
     </style>
 
 
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid justify-content-center">
-        <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-            <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
-                <a href="{{ url('/') }}" class=" justify-content-center text-center">
-                    <img alt="Logo" src="{{url('/logos/logo.png')}}" class="h-125px theme-light-show"/>
-                    <img alt="Logo" src="{{url('/logos/white.png')}}" class="h-80px theme-dark-show"/>
+    <div class="d-flex flex-column flex-lg-row flex-column-fluid justify-content-center align-items-center min-h-screen p-20 p-md-20">
+        <div class="bg-body d-flex flex-column flex-center rounded-4 w-100 p-20 p-md-20" style="max-width: 600px;"> {{-- Ziadat al-padding al-dakhili huna --}}
 
+            <a href="{{ url('/') }}" class="d-flex justify-content-center text-center mb-8"> {{-- Ziadat al-margin asfal al-logo --}}
+                <img alt="Logo" src="{{ asset('logos/logo.png') }}" class="h-70px h-md-90px theme-light-show"/> {{-- Ziadat ارتفاعat al-suwar --}}
+                <img alt="Logo" src="{{ asset('logos/white.png') }}" class="h-60px h-md-80px theme-dark-show"/> {{-- Ziadat ارتفاعat al-suwar --}}
+            </a>
+            <div class="d-flex flex-center flex-column align-items-stretch w-100" style="max-width: 400px;">
+                <div class="d-flex flex-center flex-column flex-column-fluid pb-20 pb-lg-25"> {{-- Ziadat al-padding asfal al-form --}}
+                    <form class="form w-100" novalidate="novalidate" id="login_form" method="post"
+                          action="{{ route('admin.login.submit') }}">
+                        @csrf
+                        <div class="text-center mb-10"> {{-- Ziadat al-margin --}}
+                            <h1 class="text-gray-700 fw-bolder mb-3">👋 Welcome Back</h1> {{-- Ziadat al-margin --}}
+                            <p class="text-muted">Sign in to your account</p>
+                        </div>
 
-                </a>
+                        <div class="fv-row mb-7"> {{-- Ziadat al-margin --}}
+                            <input type="text" placeholder="Email" name="email" autocomplete="off"
+                                   class="form-control bg-transparent"/>
+                            <div class="fv-plugins-message-container invalid-feedback mt-2"
+                                 data-field-error="email"></div>
+                        </div>
 
-                <div class="d-flex flex-center flex-column align-items-stretch h-50 w-md-400px ">
-                    <div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
-                        <form class="form w-100" novalidate="novalidate" id="login_form" method="post"
-                              action="{{ route('admin.login.submit') }}">
-                            @csrf
-                            <div class="text-center mb-11">
-                                <h1 class="text-gray-700 fw-bolder mb-3">👋 Welcome Back</h1>
-                                <p class="text-muted">Sign in to your account</p>
+                        <div class="fv-row mb-5"> {{-- Ziadat al-margin --}}
+                            <input type="password" placeholder="Password" name="password" autocomplete="off"
+                                   class="form-control bg-transparent"/>
+                            <div class="fv-plugins-message-container invalid-feedback mt-2"
+                                 data-field-error="password"></div>
+                        </div>
 
+                        <div class="fv-row mt-6 mb-7"> {{-- Ziadat al-spacing --}}
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="1" name="remember"
+                                       id="flexCheckDefault"/>
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Remember me
+                                </label>
                             </div>
-                            <div class="fv-row mb-8">
-                                <input type="text" placeholder="Email" name="email" autocomplete="off"
-                                       class="form-control bg-transparent"/>
-                                {{-- Container for email field error message --}}
-                                <div class="fv-plugins-message-container invalid-feedback mt-2"
-                                     data-field-error="email"></div>
-                            </div>
-                            <div class="fv-row mb-3">
-                                <input type="password" placeholder="Password" name="password" autocomplete="off"
-                                       class="form-control bg-transparent"/>
-                                {{-- Container for password field error message --}}
-                                <div class="fv-plugins-message-container invalid-feedback mt-2"
-                                     data-field-error="password"></div>
-                            </div>
+                        </div>
 
-                            <div class="fv-row mt-5 mb-5">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="1" name="remember"
-                                           id="flexCheckDefault"/>
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Remember me
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="d-grid mb-10">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                                    <span class="indicator-label">Sign In</span>
-                                    <span class="indicator-progress">Please wait...
-                                  <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="d-grid mb-10"> {{-- Ziadat al-margin --}}
+                            <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                <span class="indicator-label">Sign In</span>
+                                <span class="indicator-progress">Please wait...
+                              <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
-
             </div>
         </div>
-    </div>
-</div>
-<script>var hostUrl = "{{ url('assets/') }}/";</script>
+    </div><script>var hostUrl = "{{ url('assets/') }}/";</script>
 <script src="{{ url('admin/plugins/global/plugins.bundle.js') }}"></script>
 <script src="{{ url('admin/js/scripts.bundle.js') }}"></script>
 <script src="{{ url('admin/js/custom/login.js') }}"></script>
